ec531b921c1c9550b349925e7e3cac030a2acaa6
kautliya9211 <demorgans007@gmail.com>
2025-04-18 12:01:06 +0530

    commit

--- socialmediaanalytics/src/pages/TopUsers.jsx
+++ socialmediaanalytics/src/pages/TopUsers.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const TopUsers = () => {
-  const [topUsers, setTopUsers] = useState([]);
+  const [topUsers, setTopUsers] = useState({});
   console.log(topUsers);
+
   useEffect(() => {
     const getTopUsers = async () => {
       try {
@@ -11,12 +12,11 @@ const TopUsers = () => {
           "http://20.244.56.144/evaluation-service/users",
           {
             headers: {
-              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0OTU2OTI2LCJpYXQiOjE3NDQ5NTY2MjYsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjE3YjEyOGU2LTRlNTMtNGI1NC1hMTM0LTc5MmJjNzkwNzkxNyIsInN1YiI6Im1yYWR1bC52ZXJtYV9jczIyQGdsYS5hYy5pbiJ9LCJlbWFpbCI6Im1yYWR1bC52ZXJtYV9jczIyQGdsYS5hYy5pbiIsIm5hbWUiOiJtcmFkdWwgdmVybWEiLCJyb2xsTm8iOiIyMjE1MDAxMTAyIiwiYWNjZXNzQ29kZSI6IkNObmVHVCIsImNsaWVudElEIjoiMTdiMTI4ZTYtNGU1My00YjU0LWExMzQtNzkyYmM3OTA3OTE3IiwiY2xpZW50U2VjcmV0IjoicG1zZkFzS2FzY1dwV3ViWiJ9.YWRWdlDjrPByoOcEOBblfF-WSY8VCncArVIFNnwKrKs`,
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0OTU3OTU4LCJpYXQiOjE3NDQ5NTc2NTgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjE3YjEyOGU2LTRlNTMtNGI1NC1hMTM0LTc5MmJjNzkwNzkxNyIsInN1YiI6Im1yYWR1bC52ZXJtYV9jczIyQGdsYS5hYy5pbiJ9LCJlbWFpbCI6Im1yYWR1bC52ZXJtYV9jczIyQGdsYS5hYy5pbiIsIm5hbWUiOiJtcmFkdWwgdmVybWEiLCJyb2xsTm8iOiIyMjE1MDAxMTAyIiwiYWNjZXNzQ29kZSI6IkNObmVHVCIsImNsaWVudElEIjoiMTdiMTI4ZTYtNGU1My00YjU0LWExMzQtNzkyYmM3OTA3OTE3IiwiY2xpZW50U2VjcmV0IjoicG1zZkFzS2FzY1dwV3ViWiJ9.ayOnsfJoiR_ogZ1tBXa6xb1r6MDD81b0aJMDnM5iNxY`,
             },
           }
         );
         setTopUsers(response.data.users);
-        // console.log("Top Users:", response.data.users);
       } catch (error) {
         console.error("Error fetching top users:", error);
       }
@@ -28,7 +28,21 @@ const TopUsers = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-semibold mb-4">Top Users Page</h1>
-      <div></div>
+      <div className="space-y-4">
+        {topUsers.map((user, index) => (
+          <div
+            key={user.id || index}
+            className="bg-white shadow-md rounded-lg p-4 flex justify-between items-center"
+          >
+            <div>
+              <h2 className="text-lg font-bold">{user.name}</h2>
+            </div>
+            <span className="text-indigo-600 text-xl font-semibold">
+              #{index + 1}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- socialmediaanalytics/src/pages/TrendingPosts.jsx
+++ socialmediaanalytics/src/pages/TrendingPosts.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 const TrendingPosts = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [topPosts, setTopPosts] = useState([]);
+  console.log(topPosts);
+  useEffect(() => {
+    const getTopPosts = async () => {
+      try {
+        const response = await axios.get(
+          "http://20.244.56.144/evaluation-service/users/1/posts",
+          {
+            headers: {
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0OTU3NTM1LCJpYXQiOjE3NDQ5NTcyMzUsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjE3YjEyOGU2LTRlNTMtNGI1NC1hMTM0LTc5MmJjNzkwNzkxNyIsInN1YiI6Im1yYWR1bC52ZXJtYV9jczIyQGdsYS5hYy5pbiJ9LCJlbWFpbCI6Im1yYWR1bC52ZXJtYV9jczIyQGdsYS5hYy5pbiIsIm5hbWUiOiJtcmFkdWwgdmVybWEiLCJyb2xsTm8iOiIyMjE1MDAxMTAyIiwiYWNjZXNzQ29kZSI6IkNObmVHVCIsImNsaWVudElEIjoiMTdiMTI4ZTYtNGU1My00YjU0LWExMzQtNzkyYmM3OTA3OTE3IiwiY2xpZW50U2VjcmV0IjoicG1zZkFzS2FzY1dwV3ViWiJ9.eVeVs90qGAsR8EsBPMbXEdZRFL-oBWp3xdRry5cB0XQ`,
+            },
+          }
+        );
+        setTopPosts(response.data.posts);
+        // console.log("Top Users:", response.data.posts);
+      } catch (error) {
+        console.error("Error fetching top posts:", error);
+      }
+    };
 
-export default TrendingPosts
+    getTopPosts();
+  }, []);
+
+  return <div></div>;
+};
+
+export default TrendingPosts;
